1e886d56a6fd7102e42f8dfd5ea3c4c5461ac765
David Lukes <dafydd.lukes@gmail.com>
2018-03-16 17:24:47 +0100

    Regulární výrazy
    Touto jednoduchou změnou umožňujeme uživateli, aby vyhledával pomocí
    regulárních výrazů.
    
    Náš program se tím rázem stal mnohem užitečnějším, ale stále není příliš
    uživatelsky příjemný. Uživatel si musí skript přečíst, aby pochopil, jak
    funguje a byl schopný ho správně použít. Při nesprávném použití na něj
    pak vyskočí kryptické chyby:
    
      $ ./grep.py
      Traceback (most recent call last):
        File "./grep.py", line 14, in <module>
          pattern, path = sys.argv[1:]
      ValueError: not enough values to unpack (expected 2, got 0)
      $ ./grep.py foo bar
      Traceback (most recent call last):
        File "./grep.py", line 15, in <module>
          with open(path) as file:
      FileNotFoundError: [Errno 2] No such file or directory: 'bar'
    
    To bychom měli nějak zlepšit.

--- grep.py
+++ grep.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 pattern, path = sys.argv[1:]
 with open(path) as file:
     for line in file:
-        if pattern in line:
+        if re.search(pattern, line):
             # každý z řádků v prohledávaném souboru už na konci obsahuje znak
             # nového řádku, funkce print() by defaultně další přidala → to
             # nechceme, tak si pomocí argumentu end="" vyžádáme, aby funkce
