04d9c239fc422e3bc68f9a7b94161498500c0fd3
David Lukes <dafydd.lukes@gmail.com>
2018-03-16 17:21:33 +0100

    Minimum viable product
    Je načase, aby náš program začal dělat něco užitečného :) V tuto chvíli
    lze zavolat `./grep.py line grep.py` a program nám vytiskne všechny
    řádky, které obsahují doslova slovo "line".

--- grep.py
+++ grep.py
@@ -2,4 +2,12 @@
 
 import sys
 
-print(sys.argv)
+pattern, path = sys.argv[1:]
+with open(path) as file:
+    for line in file:
+        if pattern in line:
+            # každý z řádků v prohledávaném souboru už na konci obsahuje znak
+            # nového řádku, funkce print() by defaultně další přidala → to
+            # nechceme, tak si pomocí argumentu end="" vyžádáme, aby funkce
+            # print() další znak nového řádku už nepřidávala
+            print(line, end="")
