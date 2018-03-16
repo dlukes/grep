0df7a688701be09218fc70f3d74ebdeb5226bc72
David Lukes <dafydd.lukes@gmail.com>
2018-03-16 17:40:36 +0100

    Ošetření chyb
    Hned lepší! Nyní se program při špatném použití pokusí uživateli
    naznačit, jak se používá správně. Konkrétní postup, jak jsme toho
    docílili, je okomentovaný přímo v kódu.

--- grep.py
+++ grep.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python3
 
+# následující textový řetězec obsahuje úvodní informace ke skriptu jako celku.
+# je dostupný přes speciální proměnnou `__doc__`, čehož využijeme níže.
+"""Usage: grep.py PATTERN FILE
+
+Print lines from FILE matching regular expression PATTERN.
+
+"""
+
 import sys
 import re
 
-pattern, path = sys.argv[1:]
-with open(path) as file:
-    for line in file:
-        if re.search(pattern, line):
-            # každý z řádků v prohledávaném souboru už na konci obsahuje znak
-            # nového řádku, funkce print() by defaultně další přidala → to
-            # nechceme, tak si pomocí argumentu end="" vyžádáme, aby funkce
-            # print() další znak nového řádku už nepřidávala
-            print(line, end="")
+# jak jsme zjistili, v tomto bodě může nastat ValueError (viz předchozí commit
+# message). měli bychom ji tedy odchytit a pokud nastane, zobrazit uživateli
+# nějakou užitečnější zprávu -- třeba návod, jak program používat správně,
+# obsažený v proměnné __doc__, protože ta ValueError je prapůvodně způsobená
+# tím, že uživatel programu předal špatný počet parametrů.
+try:
+    pattern, path = sys.argv[1:]
+except ValueError:
+    # abychom nemíchali užitečný výstup programu -- profiltrované řádky ze
+    # souboru -- s chybovými hláškami, tiskneme chybové hlášky na tzv. chybový
+    # výstup (standard error → sys.stderr). užitečný výstup jde naopak na tzv.
+    # standardní výstup (standard output → sys.stdout), který u funkce print()
+    # není potřeba specifikovat, protože je to default. v terminálu se sice
+    # vytiskne obojí zároveň, ale např. k dalšímu zpracování skrz roury se
+    # posílá jen standardní výstup.
+    print(__doc__.strip(), file=sys.stderr)
+    # funkce sys.exit() okamžitě ukončí program; konvence je, že jakékoli jiné
+    # číslo než 0 (standardně se používá 1) naznačí, že běh programu neskončil
+    # úspěchem
+    sys.exit(1)
+
+# při otevírání souboru může zase nastat FileNotFoundError (jak jsme také
+# zjistili při testování předchozí verze skriptu). vypořádáme se s ní podobně:
+try:
+    with open(path) as file:
+        for line in file:
+            if re.search(pattern, line):
+                print(line, end="")
+# navíc si ale uložíme samotnou chybu do proměnné err a vytiskneme ji taky,
+# abychom uživateli dali bližší nápovědu, v čem je problém
+except FileNotFoundError as err:
+    print(__doc__.strip(), file=sys.stderr)
+    print(err, file=sys.stderr)
+    sys.exit(1)
