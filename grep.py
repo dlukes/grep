4890ddaae33bde1a1b79770f2dade5c87d04eadb
David Lukes <dafydd.lukes@gmail.com>
2018-03-19 10:32:34 +0100

    Obalit kód do funkcí
    Aby byl kód přehlednější a šel testovat po částech, je v této fázi dobré
    ho rozdělit do funkcí s jasně daným účelem, které spolu spolupracují,
    ale jdou v interaktivní konzoli spouštět i nezávisle.
    
    Funkce `grep` představuje jádro funkcionality programu. Umí procházet
    řádky a vytisknout ty, které matchují nějaký regulární výraz.
    
    Funkce `parse_argv` umí zkontrolovat, zda uživatel předal programu
    správný počet argumentů.
    
    Funkce `main` tyto funkce propojuje a stará se o běh celého skriptu.
    Tím, že ji na posledním řádku skriptu zavoláme, se vlastně celý skript
    teprve začne vykonávat (do té doby jsme akorát importovali moduly a
    definovali funkce). Také se stará o případné chyby, které mohou nastat,
    zobrazuje uživateli nápovědu atp.
    
    Co jsme tímto krokem získali? Veškerou složitou interakci s venkovním
    světem (uživatelem) jsme obalili do funkce `main`. Funkce `grep` a
    `parse_argv` počítají jen s ideálním případem, kdy je vše, jak má, a
    díky tomu jsou kratší, přehlednější, v případě potřeby se budou lépe
    upravovat.

--- grep.py
+++ grep.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# následující textový řetězec obsahuje úvodní informace ke skriptu jako celku.
-# je dostupný přes speciální proměnnou `__doc__`, čehož využijeme níže.
 """Usage: grep.py PATTERN FILE
 
 Print lines from FILE matching regular expression PATTERN.
@@ -11,37 +9,30 @@ Print lines from FILE matching regular expression PATTERN.
 import sys
 import re
 
-# jak jsme zjistili, v tomto bodě může nastat ValueError (viz předchozí commit
-# message). měli bychom ji tedy odchytit a pokud nastane, zobrazit uživateli
-# nějakou užitečnější zprávu -- třeba návod, jak program používat správně,
-# obsažený v proměnné __doc__, protože ta ValueError je prapůvodně způsobená
-# tím, že uživatel programu předal špatný počet parametrů.
-try:
-    pattern, path = sys.argv[1:]
-except ValueError:
-    # abychom nemíchali užitečný výstup programu -- profiltrované řádky ze
-    # souboru -- s chybovými hláškami, tiskneme chybové hlášky na tzv. chybový
-    # výstup (standard error → sys.stderr). užitečný výstup jde naopak na tzv.
-    # standardní výstup (standard output → sys.stdout), který u funkce print()
-    # není potřeba specifikovat, protože je to default. v terminálu se sice
-    # vytiskne obojí zároveň, ale např. k dalšímu zpracování skrz roury se
-    # posílá jen standardní výstup.
-    print(__doc__.strip(), file=sys.stderr)
-    # funkce sys.exit() okamžitě ukončí program; konvence je, že jakékoli jiné
-    # číslo než 0 (standardně se používá 1) naznačí, že běh programu neskončil
-    # úspěchem
-    sys.exit(1)
+def grep(pattern, lines):
+    """Print lines matching pattern."""
+    for line in lines:
+        if re.search(pattern, line):
+            print(line, end="")
 
-# při otevírání souboru může zase nastat FileNotFoundError (jak jsme také
-# zjistili při testování předchozí verze skriptu). vypořádáme se s ní podobně:
-try:
-    with open(path) as file:
-        for line in file:
-            if re.search(pattern, line):
-                print(line, end="")
-# navíc si ale uložíme samotnou chybu do proměnné err a vytiskneme ji taky,
-# abychom uživateli dali bližší nápovědu, v čem je problém
-except FileNotFoundError as err:
-    print(__doc__.strip(), file=sys.stderr)
-    print(err, file=sys.stderr)
-    sys.exit(1)
+def parse_argv(argv):
+    """Parse script arguments."""
+    pattern, path = argv
+    return pattern, path
+
+def main(argv):
+    """Main entry point of script."""
+    try:
+        pattern, path = parse_argv(argv)
+    except ValueError:
+        print(__doc__.strip(), file=sys.stderr)
+        sys.exit(1)
+    try:
+        with open(path) as file:
+            grep(pattern, file)
+    except FileNotFoundError as err:
+        print(__doc__.strip(), file=sys.stderr)
+        print(err, file=sys.stderr)
+        sys.exit(1)
+
+main(sys.argv[1:])
