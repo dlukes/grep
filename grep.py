e0c2c942fd59b659d2c9917754529a83f640b063
David Lukes <dafydd.lukes@gmail.com>
2018-03-19 11:47:46 +0100

    Zpracování parametrů pomocí modulu argparse
    U složitějších skriptů, které mají větší množství parametrů (včetně
    přepínačů) se vyplatí jejich parsování přenechat specializovanému modulu
    `argparse`. Tento modul nabízí možnost nadefinovat různé standardní typy
    parametrů, tak jak jsou na ně uživatelé zvyklí z jiných programů pro
    příkazovou řádku a sám je konzistentně a spolehlivě naparsuje, případně
    se postará o předčasné ukončení skriptu, pokud uživatel na nějaké
    povinné parametry zapomněl. Navíc automatický vytvoří hezky
    zformátovanou nápovědu s přehledem všech dostupných parametrů a namapuje
    ji na přepínač `-h`/`--help`:
    
      $ ./grep.py -h
      usage: grep.py [-h] PATTERN FILE [FILE ...]
    
      Print lines from each FILE matching regular expression PATTERN.
    
      positional arguments:
        PATTERN     Pattern to match
        FILE        Files to search (- for standard input)
    
      optional arguments:
        -h, --help  show this help message and exit
    
    Základní představu o používání modulu `argparse` si můžete udělat z
    kódu, detailnější přehled, včetně různých typů parametrů, které můžete
    definovat, najdete zde: https://docs.python.org/3/howto/argparse.html

--- grep.py
+++ grep.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-"""Usage: grep.py PATTERN FILE [FILE...]
-
-Print lines from each FILE matching regular expression PATTERN. Pass `-` for
-standard input.
+"""Print lines from each FILE matching regular expression PATTERN.
 
 """
 
 import sys
 import fileinput as fi
 import logging as log
+import argparse
 import re
 
-# modul logging umožňuje rozčlenit zprávy uživateli do různých úrovní
-# důležitosti. v pořadí od nejméně důležité to jsou DEBUG, INFO, WARNING, ERROR
-# a CRITICAL. defaultně se zobrazují jen zprávy s důležitostí WARNING a vyšší;
-# pokud chceme jít níž, musíme si to nakonfigurovat:
 log.basicConfig(level=log.INFO)
 
 
@@ -27,27 +21,25 @@ def grep(pattern, lines):
 
 def parse_argv(argv):
     """Parse script arguments."""
-    # pokud dostaneme míň než dva parametry, ručně vyvoláme ValueError
-    if len(argv) < 2:
-        raise ValueError
-    # jinak bereme první parametr jako pattern a veškeré zbývající jako cesty k
-    # souborům na prohledání
-    pattern, paths = argv[0], argv[1:]
-    return pattern, paths
+    parser = argparse.ArgumentParser(description=__doc__.strip())
+    parser.add_argument("pattern", metavar="PATTERN", type=str, help="Pattern to match")
+    parser.add_argument(
+        "paths",
+        metavar="FILE",
+        type=str,
+        nargs="+",
+        help="Files to search (- for standard input)",
+    )
+    return parser.parse_args(argv)
 
 
 def main(argv):
     """Main entry point of script."""
+    args = parse_argv(argv)
+    log.info(f"Searching for {args.pattern!r} in {args.paths!r}.")
     try:
-        pattern, paths = parse_argv(argv)
-    except ValueError:
-        log.critical(__doc__.strip())
-        sys.exit(1)
-    log.info(f"Searching for {pattern!r} in {paths!r}.")
-    try:
-        grep(pattern, fi.input(paths))
+        grep(args.pattern, fi.input(args.paths))
     except FileNotFoundError as err:
-        log.critical(__doc__.strip())
         log.critical(err)
         sys.exit(1)
 
