0c8a18058f98cfb72948497b1de94e57c7f1bbe1
David Lukes <dafydd.lukes@gmail.com>
2018-03-19 10:52:32 +0100

    Automatická úprava formátování
    Pro psaní programů v Pythonu existují různé stylistické konvence, které
    na běh samotného programu nemají žádný vliv, ale je zvykem je
    respektovat (viz např. https://www.python.org/dev/peps/pep-0008). Jenže
    kdo si to má všechno pamatovat? Naštěstí existují programy, které to
    ohlídají za nás!
    
    `flake8` projde skript a ohlásí různé stylistické prohřešky. Navíc je
    také schopný upozornit na různé "opravdové" chyby, např. kdyby se náš
    skript snažil použít proměnnou, která není definovaná.
    
      $ flake8 grep.py
      grep.py:12:1: E302 expected 2 blank lines, found 1
      grep.py:18:1: E302 expected 2 blank lines, found 1
      grep.py:23:1: E302 expected 2 blank lines, found 1
      grep.py:38:1: E305 expected 2 blank lines after class or function definition, found 1
    
    Opravovat stylistické prohřešky ručně, zejména pokud máte kódu hodně,
    pochopitelně není příliš velká zábava. Tím pádem jsou i programy, které
    to za vás udělají automaticky, aby na to člověk nemusel myslet. V
    současnosti se pro tyto účely často používá program `black` (viz
    https://github.com/psf/black). Měli jsme ve skriptu pár "prohřešků", tak
    je pomocí `black` opravíme:
    
      $ black grep.py
      reformatted grep.py
      All done! ✨ 🍰 ✨
      1 file reformatted.
    
    Změny se rovnou zapíšou do původního souboru, takže už stačí jen
    commitnout.

--- grep.py
+++ grep.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Usage: grep.py PATTERN FILE
 
 Print lines from FILE matching regular expression PATTERN.
@@ -9,16 +8,19 @@ Print lines from FILE matching regular expression PATTERN.
 import sys
 import re
 
+
 def grep(pattern, lines):
     """Print lines matching pattern."""
     for line in lines:
         if re.search(pattern, line):
             print(line, end="")
 
+
 def parse_argv(argv):
     """Parse script arguments."""
     pattern, path = argv
     return pattern, path
+
 
 def main(argv):
     """Main entry point of script."""
@@ -34,6 +36,7 @@ def main(argv):
         print(__doc__.strip(), file=sys.stderr)
         print(err, file=sys.stderr)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
